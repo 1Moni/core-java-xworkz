b7df8b6fa539f691fa25c01c915cb7b527c03d2f
1Moni <monishar39472@gmail.com>
2023-07-19 19:26:52 +0530

    commiting presentation related files

---
+++ main.java
@@ -0,0 +1,12 @@
+class main{
+static int age[]={12,43,56,10};
+public static void main(String args[]){
+System.out.println("using for loop");
+for(int i=0;i<=age.length;i++){
+System.out.println(age[i]);
+
+}
+
+}
+
+}
